480953598d78e539bf3f9e651f2a53fe39b757f2
Mitch Partee <mitch.partee@k12.wa.us>
2022-11-03 11:31:02 -0700

    Module 4 fix

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -40,7 +40,11 @@ namespace GradeBook.GradeBooks
             {
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
             }
-            base.CalculateStatistics();
+            else
+            {
+                base.CalculateStatistics();
+            }
+            
         }
         public override void CalculateStudentStatistics(string name)
         {
@@ -48,7 +52,11 @@ namespace GradeBook.GradeBooks
             {
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
             }
-            base.CalculateStudentStatistics(name);
+            else
+            {
+                base.CalculateStudentStatistics(name);
+            }
+            
         }
     }
 }
